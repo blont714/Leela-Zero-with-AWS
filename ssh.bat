8677ad660f9892b0ac1c9312348e845bba1bb0d2
blont714 <33482172+blont714@users.noreply.github.com>
2019-08-29 15:02:01 +0900

    Update ssh.bat

--- ssh.bat
+++ ssh.bat
@@ -1,7 +1,7 @@
 @echo off
 set plink_path="C:\Program Files\PuTTY\plink.exe"
-set key_path="C:\Users\BLOND\Desktop\AWS\lizzie\aws-key01.ppk"
-set server_ip=13.114.63.201
+set key_path="C:\Users\#User#\Desktop\AWS\lizzie\aws-key01.ppk"
+set server_ip=#ip_address#
 
 echo yes | %plink_path% -ssh -i %key_path% ubuntu@%server_ip% exit
 %plink_path% -ssh -i %key_path% ubuntu@%server_ip% cd leela-zero; ./leelaz --gtp --lagbuffer 0 --weights elf.gz --threads 2
