9aee02711fa295569b4192b8dc23ad3cae8fb2ff
blont714 <33482172+blont714@users.noreply.github.com>
2019-08-29 16:14:53 +0900

    Update ssh.bat

--- ssh.bat
+++ ssh.bat
@@ -1,6 +1,6 @@
 @echo off
 set plink_path="C:\Program Files\PuTTY\plink.exe"
-set key_path="C:\Users\#User#\Desktop\lizzie\aws-key01.ppk"
+set key_path="C:\Users\#User#\Desktop\\aws-key01.ppk"
 set server_ip=#ip_address#
 
 echo yes | %plink_path% -ssh -i %key_path% ubuntu@%server_ip% exit
